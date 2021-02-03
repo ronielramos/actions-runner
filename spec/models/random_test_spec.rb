41830b5461d167199bf1bd3a6ffb999df9432f0f
Roniel <roniel_ramos@hotmail.com>
2021-02-03 13:55:30 -0300

    test 01

---
+++ spec/models/random_test_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe('Random') do
+  describe "Dummy test" do
+    it "Success" do
+      expect(true).to be(true)
+    end
+
+    it "Failure" do
+      expect(true).to be(false)
+    end
+  end
+end
