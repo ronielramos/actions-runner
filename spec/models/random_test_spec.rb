496b443feffeeb0b07fb714bd1e7dd089f5c5758
Roniel <roniel_ramos@hotmail.com>
2021-02-03 19:33:08 -0300

    run rubocop

--- spec/models/random_test_spec.rb
+++ spec/models/random_test_spec.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe('Random') do
-  describe "Dummy test" do
-    it "Success" do
+  describe 'Dummy test' do
+    it 'Success' do
       expect(true).to be(true)
     end
   end
